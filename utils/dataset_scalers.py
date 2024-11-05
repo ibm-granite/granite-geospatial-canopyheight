4ccab30184045508679c0c62de34bbddd45353ca
Julian-Kuehnert <julian.kuehnert@ibm.com>
2024-11-05 09:02:42 -0500

    memory efficient version of scaler calculation

--- utils/dataset_scalers.py
+++ utils/dataset_scalers.py
@@ -11,16 +11,33 @@ def calc_mean_std(img_folder):
         Returns:
             Pandas Dataframe with mean and std for all available bands in input data. 
     """
-    img_lst = []
 
+    count = 0
+    mean = np.zeros(10)
     for img_file in os.listdir(img_folder):
         if '.tif' in img_file:
-            imm = rio.open(os.path.join(img_folder,img_file)).read().astype('float32')
-            img_lst.append(imm)
+            with rio.open(os.path.join(img_folder,img_file)) as f:
+              imm = f.read().astype('float32')
+            c, h, w = imm.shape
+            imm[imm==-9999] = np.nan
+            nb_pixels = h * w - (np.count_nonzero(np.isnan(imm)))
+            sum = np.nansum(imm, axis=(1,2))
+            mean = (count * mean + sum) / (count + nb_pixels)
+            count += nb_pixels
 
-    img_lst_arr = np.array(img_lst)
-    img_lst_arr[img_lst_arr==-9999] = np.nan
-    mean = np.nanmean(img_lst_arr, axis=(0,2,3))
-    std = np.nanstd(img_lst_arr, axis=(0,2,3))
+    count = 0
+    variance = np.zeros_like(mean)
+    for img_file in os.listdir(img_folder):
+        if '.tif' in img_file:
+            with rio.open(os.path.join(img_folder,img_file)) as f:
+              imm = f.read().astype('float32')
+            c, h, w = imm.shape
+            imm[imm==-9999] = np.nan
+            nb_pixels = h * w - (np.count_nonzero(np.isnan(imm)))
+            sum_squared = np.nansum((imm.transpose(1,2,0)-mean)**2, axis=(0,1))
+            variance = (count * variance + sum_squared) / (count + nb_pixels)
+            count += nb_pixels
+
+    std = np.sqrt(variance)
 
     return( pd.DataFrame({'mean': mean,'std': std}) )
